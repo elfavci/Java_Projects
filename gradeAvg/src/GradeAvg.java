3534a07f6c7c5aa75cabe7383f081c34b45d793d
Elif Avcı <elffavci@gmail.com>
2023-03-23 16:40:52 +0300

    Not Ortalaması Hesaplayan Program
    Java ile not ortalaması hesaplayan eğer kullanıcının ortalaması 60'ın üzerindeyse "Sınıfı geçti",küçük ise "Sınıfta kaldı" yazsın. If else yapısı kullanmadan yap

---
+++ gradeAvg/src/GradeAvg.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class GradeAvg {
+
+	public static void main(String[] args) {
+		Scanner input =new Scanner(System.in);
+		
+		int mat,fiz,kim,tur,tar,muz;
+		float avg;
+		System.out.print("Matemetik ders notu:");
+		mat=input.nextInt();
+		System.out.print("Fizik ders notu:");
+		fiz=input.nextInt();
+		System.out.print("Kimya ders notu:");
+		kim=input.nextInt();
+		System.out.print("Turkce ders notu:");
+		tur=input.nextInt();
+		System.out.print("Tarih ders notu:");
+		tar=input.nextInt();
+		System.out.print("Muzik ders notu:");
+		muz=input.nextInt();
+		
+		avg= (mat+fiz+kim+tur+tar+muz)/6;
+		System.out.print((avg>60)?"Sinifi gecti":"Sinifta kaldi");
+		
+
+	}
+
+}
