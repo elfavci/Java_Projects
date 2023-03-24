f1b990f66cfbb69ec97276e29572e6f1c97c8e48
Elif Avcı <elffavci@gmail.com>
2023-03-24 16:02:14 +0300

    Taksimetre Programı
    Java ile gidilen mesafeye (KM) göre taksimetre tutarını ekrana yazdıran programı yazın.
    
    Taksimetre KM başına 2.20 TL tutmaktadır.
    Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
    Taksimetre açılış ücreti 10 TL'dir.

---
+++ taxi/src/Taxi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Taxi {
+
+	public static void main(String[] args) {
+		double price=2.20,sonuc,op=10;
+		int km;
+		Scanner in=new Scanner(System.in);
+		System.out.println("kilometre:");
+		km=in.nextInt();
+		sonuc= op+price*km;
+		if(sonuc>20) {
+			System.out.println(sonuc+" TL odenecek");
+		}
+		else {
+			System.out.println("20 TL odenecek");
+		}
+	}
+
+}
