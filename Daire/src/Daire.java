77baa43a2d6bb8b692c6e61f0ed4c7fa7ad06ec7
Elif Avcı <elffavci@gmail.com>
2023-03-24 16:21:34 +0300

    Dairenin alanı
    Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
    
    𝜋 sayısını = 3.14 alınız.
    
    Formül : (𝜋 * (r*r) * 𝛼) / 360
    https://www.patika.dev/tr

---
+++ Daire/src/Daire.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Daire {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		double r,a,alan,pi=3.14;
+		System.out.println("dairenin yariçapini girin:");
+		r=in.nextDouble();
+		System.out.println("dairenin merkez açısını girin:");
+		a=in.nextDouble();
+		alan=(pi*(r*r)*a)/360;
+		System.out.println(alan);
+	}
+}
