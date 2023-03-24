3be9490864e884d93d509dffc03868ddfff98d8f
Elif Avcı <elffavci@gmail.com>
2023-03-24 16:33:01 +0300

    Vücut Kitle İndeksi Hesaplama
    Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın. Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
    https://www.patika.dev/tr

---
+++ Vkitle/src/Kitle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Kitle {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("kilonuzu girin:");
+		int kilo = scanner.nextInt();
+		System.out.print("boyunuzu girin:(metre cinsinden ornek 1,72)");
+		double boy = scanner.nextDouble();
+		
+		double bki = kilo/(boy*boy);
+		System.out.print("beden kitle indeksiniz:" + bki);
+	}
+}
