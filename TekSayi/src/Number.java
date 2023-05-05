119026a5ffe50241e9c76d6d1c184b5b7d60cccc
Elif Avcı <elffavci@gmail.com>
2023-05-05 15:17:41 +0300

    Tek Sayıların Toplamını Program
    Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan program

---
+++ TekSayi/src/Number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Number {
+
+	public static void main(String[] args) {
+		int n,total=0;
+		Scanner input=new Scanner(System.in);
+		do {
+			System.out.println("Enter the number:");
+			n=input.nextInt();
+			if(n%4==0) {
+				total=total+n;
+			}
+		}while(n%2!=1);
+		System.out.println("Total = " +total);
+		input.close();
+
+	}
+
+}
