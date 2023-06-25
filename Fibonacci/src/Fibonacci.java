4437d172da3cbea3f32cca6a4c200eea97a9fd02
Elif Avcı <elffavci@gmail.com>
2023-06-26 00:51:36 +0300

    Fibonacci Serisi
    Fibonacci serisi, her sayının kendinden önceki ile toplanması sonucu oluşan bir sayı dizisidir. Bu şekilde devam eden bu dizide sayılar birbirleriyle oranlandığında altın oran ortaya çıkar, yani bir sayı kendisinden önceki sayıya bölündüğünde altın orana gittikçe yaklaşan bir dizi elde edilir.
    
    Fibonacci dizisi, 0'dan başlar ve sonsuza kadar. Her rakam, bir önceki rakamla toplanır. Elde edilen sonuç rakamın sağ tarafına yazılır. Fibonacci dizisinin ilk on sayısı şu şekildedir:
    
    9 Elemanlı Fibonacci Serisi : 0 1 1 2 3 5 8 13 21 34

---
+++ Fibonacci/src/Fibonacci.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Scanner inp=new Scanner(System.in);
+		int number,total=0,first=0,second=1,i;
+		System.out.print("Number of elements: ");
+		number=inp.nextInt();
+		inp.close();
+		System.out.print(first+" "+second);
+		for(i=2;i<=number;i++) {
+			total=first+second;
+			System.out.print(" "+total);
+			first=second;
+			second=total;
+		}	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
